a31f78f496739ed537e201bcbf0e49e3b9cdfb98
Matthieu Meignan <matthieu.meignan@inolib.com>
2023-10-05 14:05:17 +0200

    feat: Stripe webhook

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
 import { useGraphQLModules } from "@envelop/graphql-modules";
 import { PrismaClient } from "@prisma/client";
+import express from "express";
 import { createApplication } from "graphql-modules";
 import { createYoga } from "graphql-yoga";
 import { createTransport } from "nodemailer";
 import Stripe from "stripe";
 
-import express from "express";
-
 import { modules } from "./modules";
+
+const prisma = new PrismaClient();
+
+const stripe = new Stripe(
+  "sk_test_51Nuz8EK4mWBX9EWw2LRndsz6eOfYkf3wqsvuLPe7QJxq3Bti8d7YEMsPRIRdNw7wVPX7N3GitJSsaVFqqQSBXbw4006i6DrqnF",
+  { apiVersion: "2023-08-16" },
+);
 
 const yoga = createYoga({
   plugins: [useGraphQLModules(createApplication({ modules }))],
@@ -21,13 +27,8 @@ const yoga = createYoga({
         pass: process.env.MAILER_PASS,
       },
     }),
-    prisma: new PrismaClient(),
-    stripe: new Stripe(
-      "sk_test_51Nuz8EK4mWBX9EWw2LRndsz6eOfYkf3wqsvuLPe7QJxq3Bti8d7YEMsPRIRdNw7wVPX7N3GitJSsaVFqqQSBXbw4006i6DrqnF",
-      {
-        apiVersion: "2023-08-16",
-      },
-    ),
+    prisma,
+    stripe,
   },
   cors: {
     allowedHeaders: ["Content-Type"],
@@ -43,9 +44,39 @@ const app = express();
 // eslint-disable-next-line @typescript-eslint/no-misused-promises
 app.use("/graphql", yoga);
 
-app.post("/stripe/webhook", (request, response) => {
-  // TODO
-});
+app.post(
+  "/stripe/webhook",
+  express.json({ type: "application/json" }),
+  (request, response) => {
+    console.log("hooked");
+
+    void (async () => {
+      const event = request.body as Stripe.Event;
+
+      switch (event.type) {
+        case "payment_intent.succeeded": {
+          const paymentIntent = event.data.object as Stripe.PaymentIntent;
+
+          await prisma.booking.create({
+            data: {
+              datetime: paymentIntent.metadata.datetime,
+              email: paymentIntent.metadata.email,
+              firstName: paymentIntent.metadata.firstName,
+              lastName: paymentIntent.metadata.lastName,
+              organization: paymentIntent.metadata.organization,
+              organizationTitle: paymentIntent.metadata.organizationTitle,
+              tel: paymentIntent.metadata.tel,
+            },
+          });
+
+          break;
+        }
+      }
+
+      response.send();
+    })();
+  },
+);
 
 export const viteNodeApp = app;
 export default app;
--- src/modules/payment/resolvers.ts
+++ src/modules/payment/resolvers.ts
@@ -2,58 +2,19 @@ import type { Resolvers } from "./types";
 
 export const resolvers: Resolvers = {
   Mutation: {
-    createBooking: async (_, args, context) => {
-      const booking = await context.prisma.booking.upsert({
-        where: {
-          id: args.id ?? "",
-        },
-        update: {
-          datetime: args.datetime,
-          firstName: args.firstName,
-          lastName: args.lastName,
-          organization: args.organization,
-          organizationTitle: args.organizationTitle,
-          email: args.email,
-          tel: args.tel,
-        },
-        create: {
-          datetime: args.datetime,
-          firstName: args.firstName,
-          lastName: args.lastName,
-          organization: args.organization,
-          organizationTitle: args.organizationTitle,
-          email: args.email,
-          tel: args.tel,
-        },
-      });
-
-      console.log("createBooking", booking.id);
-
-      return booking;
-    },
-
-    deleteBooking: async (_, args, context) => {
-      const booking = await context.prisma.booking.delete({
-        where: {
-          id: args.id,
-        },
-      });
-
-      console.log("deleteBooking", booking.id);
-
-      return booking;
-    },
-
     createPaymentIntent: async (_, args, context) => {
-      console.log("createPaymentIntent");
-
-      const customer = await context.stripe.customers.create();
-
       const paymentIntent = await context.stripe.paymentIntents.create({
         amount: 8400,
-        currency: "EUR",
-        customer: customer.id,
-        setup_future_usage: "off_session",
+        currency: "eur",
+        metadata: {
+          datetime: args.datetime,
+          email: args.email,
+          firstName: args.firstName,
+          lastName: args.lastName,
+          organization: args.organization,
+          organizationTitle: args.organizationTitle,
+          tel: args.tel,
+        },
       });
 
       return {
