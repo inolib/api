7652ea9775f2f2511fcef4ac88e502c2aeb3cbbc
Matthieu Meignan <matthieu.meignan@inolib.com>
2023-10-06 03:53:10 +0200

    chore: remove tailwindcss

--- .config/prettier/tailwindcss/prettier.config.js
+++
@@ -1,8 +0,0 @@
-import config from "../../../prettier.config.js";
-
-/** @type {import('prettier-plugin-tailwindcss').PluginOptions} */
-export default {
-  ...config,
-  plugins: ["prettier-plugin-tailwindcss"],
-  tailwindFunctions: ["clsx", "cn", "twMerge"],
-};
